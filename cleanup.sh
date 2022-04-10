6677f1e27c27ecc4035af4f8603ecb094bce598e
asadfaiz786 <102994641+asadfaiz786@users.noreply.github.com>
2022-04-10 14:30:48 -0400

    cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+rm -f C:\Users\asadf\qa2\*
+echo "All Files Deleted"
+Echo "Enjoy your Day"
+echo " Test File Generate in Sh Script" >> FilewithinFile.txt
+echo "after understanding git commad commit"
+echo "Lulu Copy"
+
+
+date
+
